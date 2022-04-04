9f5c81e867d0c9a01c073d0846dd5ff767756cfe
16365506 <16365506@L815WD220029.edu.cbe.ab.ca>
2022-04-04 13:49:25 -0600

    pff

--- Credit3/src/Exercises/Exercise2.java
+++ Credit3/src/Exercises/Exercise2.java
@@ -5,7 +5,7 @@ public class Exercise2
 
 	public static void main(String[] args) 
 	{
-	System.out.println("Hello World/r");
+	System.out.println("Hello World");
 	// single line comment! //
 	/* multi
 	   line
