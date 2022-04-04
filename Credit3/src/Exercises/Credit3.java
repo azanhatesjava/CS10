c54652f0a640fef07351efcb04dd366feb8d5d33
16365506 <16365506@L815WD220029.edu.cbe.ab.ca>
2022-04-04 13:05:09 -0600

    hm

--- Credit3/src/Exercises/Credit3.java
+++ Credit3/src/Exercises/Credit3.java
@@ -1,9 +1,21 @@
 package Exercises;
 
-public class Credit3 {
+public class Credit3 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		int grade; // DECLARATION; integer = numbers
+		String name; // String = string/line of text
+		double price; // double = decimals
+		char firstInitial; // char = characters
+		
+		grade = 87; // initialization
+		System.out.println(grade);
+		
+		name = "Azan Kamran";
+		System.out.println(name.length());
+		System.out.println(name.substring(4));
 
 	}
 
