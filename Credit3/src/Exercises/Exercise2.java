43cb2ed9d4392d68bf138975f71f60fc8c22b21e
16365506 <16365506@L815WD220029.edu.cbe.ab.ca>
2022-04-04 13:49:17 -0600

    lol

---
+++ Credit3/src/Exercises/Exercise2.java
@@ -0,0 +1,17 @@
+package Exercises;
+
+public class Exercise2 
+{
+
+	public static void main(String[] args) 
+	{
+	System.out.println("Hello World/r");
+	// single line comment! //
+	/* multi
+	   line
+	   comment
+	   !
+	 */
+	}
+
+}
